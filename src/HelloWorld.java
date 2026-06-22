d99cbf8854086be40c66b96640c0376649f744f5
Himalaya U <himalayau835@gmail.com>
2026-06-22 11:52:30 +0530

    Day 1: Java Basics, JVM, Variables and First Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int age = "Twenty";
+        int quantity=2;
+        String productName="soap";
+
+
+        System.out.println(price);
+        System.out.println(quantity);
+        System.out.println(productName);
+    }
+}
