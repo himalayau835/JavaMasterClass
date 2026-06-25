558caf708fd7a32e1ec4b3c6613b28f00f742659
Himalaya U <himalayau835@gmail.com>
2026-06-25 09:38:05 +0530

    Day 2-Operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,30 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int age = "Twenty";
-        int quantity=2;
-        String productName="soap";
+        int num1 = 20;
+        int num2=6;
+        int sum=num1+num2;
+        int difference=num1-num2;
+        int product=num1*num2;
+        int Quotient=num1/num2;
+        int remainder=num1%num2;
 
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(Quotient);
+        System.out.println(remainder);
 
-        System.out.println(price);
-        System.out.println(quantity);
-        System.out.println(productName);
+        int age=17;
+        if(age>18)
+            System.out.println("Eligible to Vote");
+        else
+            System.out.println("Not Eligible to Vote");
+
+        int number=68;
+        if(number>50 && number %2==0)
+            System.out.println("Valid Number");
+        else
+            System.out.println("Invalid Number");
     }
 }
